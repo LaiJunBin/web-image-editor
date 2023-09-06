b91a8073d87bdba12e94dd1bc59019d36abcb474
laijunbin <xyz607xx@gmail.com>
2023-09-06 19:40:26 +0800

    chore: add base path config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/web-image-editor',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
