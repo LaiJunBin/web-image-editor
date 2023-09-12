0d897e3e1a4ea46c9ef6476fe8515eae84012af1
laijunbin <xyz607xx@gmail.com>
2023-09-12 08:17:17 +0800

    fix: block z-index

--- src/components/controls/TheCursorControl.vue
+++ src/components/controls/TheCursorControl.vue
@@ -32,93 +32,97 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div v-if="block">
-    <div
-      class="absolute"
-      :style="{
-        top: 0 + 'px',
-        left: 0 + 'px',
-        transform: `rotate(${block.angle}rad) scale(${settings.scale / 100})`,
-        transformOrigin: `${blockX + (block.width * (settings.scale / 100)) / 2}px ${
-          blockY + (block.height * (settings.scale / 100)) / 2
-        }px`
-      }"
-    >
+  <Teleport to="body">
+    <div v-if="block">
       <div
-        class="absolute z-40 border-2 border-neutral-500"
+        class="absolute"
         :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + 'px',
-          width: block.width + 'px',
-          height: block.height + 'px'
+          top: 0 + 'px',
+          left: 0 + 'px',
+          transform: `rotate(${block.angle}rad) scale(${settings.scale / 100})`,
+          transformOrigin: `${blockX + (block.width * (settings.scale / 100)) / 2}px ${
+            blockY + (block.height * (settings.scale / 100)) / 2
+          }px`
         }"
-        @mousedown="tool.moving = true"
-      ></div>
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nwse-resize border border-neutral-500"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + 'px'
-        }"
-        @mousedown="tool.resizing = 1"
-      ></div>
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nesw-resize border border-neutral-500"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 'px'
-        }"
-        @mousedown="tool.resizing = 2"
-      ></div>
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nwse-resize border border-neutral-500"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 'px'
-        }"
-        @mousedown="tool.resizing = 3"
-      ></div>
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nesw-resize border border-neutral-500"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + 'px'
-        }"
-        @mousedown="tool.resizing = 4"
-      ></div>
+      >
+        <div
+          class="absolute z-40 border-2 border-neutral-500"
+          :style="{
+            top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + 'px',
+            width: block.width + 'px',
+            height: block.height + 'px'
+          }"
+          @mousedown="tool.moving = true"
+        ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nwse-resize border border-neutral-500"
+          :style="{
+            top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + 'px'
+          }"
+          @mousedown="tool.resizing = 1"
+        ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nesw-resize border border-neutral-500"
+          :style="{
+            top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 'px'
+          }"
+          @mousedown="tool.resizing = 2"
+        ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nwse-resize border border-neutral-500"
+          :style="{
+            top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 'px'
+          }"
+          @mousedown="tool.resizing = 3"
+        ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-nesw-resize border border-neutral-500"
+          :style="{
+            top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + 'px'
+          }"
+          @mousedown="tool.resizing = 4"
+        ></div>
 
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 - 10 + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 - 10 + 'px'
-        }"
-        @mousedown="tool.rotating = true"
-      ></div>
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 - 10 + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 10 + 'px'
-        }"
-        @mousedown="tool.rotating = true"
-      ></div>
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 10 + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 10 + 'px'
-        }"
-        @mousedown="tool.rotating = true"
-      ></div>
-      <div
-        class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
-        :style="{
-          top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 10 + 'px',
-          left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 - 10 + 'px'
-        }"
-        @mousedown="tool.rotating = true"
-      ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
+          :style="{
+            top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 - 10 + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 - 10 + 'px'
+          }"
+          @mousedown="tool.rotating = true"
+        ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
+          :style="{
+            top: blockY + (block.height * (settings.scale / 100 - 1)) / 2 - 10 + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 10 + 'px'
+          }"
+          @mousedown="tool.rotating = true"
+        ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
+          :style="{
+            top:
+              blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 10 + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 + block.width + 10 + 'px'
+          }"
+          @mousedown="tool.rotating = true"
+        ></div>
+        <div
+          class="absolute z-50 h-3 w-3 -translate-x-1/2 -translate-y-1/2 cursor-grab"
+          :style="{
+            top:
+              blockY + (block.height * (settings.scale / 100 - 1)) / 2 + block.height + 10 + 'px',
+            left: blockX + (block.width * (settings.scale / 100 - 1)) / 2 - 10 + 'px'
+          }"
+          @mousedown="tool.rotating = true"
+        ></div>
+      </div>
     </div>
-  </div>
+  </Teleport>
 </template>
